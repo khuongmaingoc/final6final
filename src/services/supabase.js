a96e1c17eac0a7ba2a7260014202e84456d812c7
khuongmn <mai.ngoc.khuong@usol-v.vn>
2026-09-03 15:20:18 +0700

    supper base

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,8 +1,19 @@
 import { createClient } from "@supabase/supabase-js";
 
-export const supabaseUrl = "https://dclaevazetcjjkrzczpc.supabase.co";
+//export const supabaseUrl = "https://dclaevazetcjjkrzczpc.supabase.co";
+
+
+// const supabaseKey =
+//   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRjbGFldmF6ZXRjamprcnpjenBjIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODMyOTIzNDQsImV4cCI6MTk5ODg2ODM0NH0.LGg0M-taoHgKtxCzr9owrb09epnPaO_Yfz6xVE54sIY";
+
+export const supabaseUrl = "https://wglexvrxjqbkyhxomsid.supabase.co";
 const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRjbGFldmF6ZXRjamprcnpjenBjIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODMyOTIzNDQsImV4cCI6MTk5ODg2ODM0NH0.LGg0M-taoHgKtxCzr9owrb09epnPaO_Yfz6xVE54sIY";
-const supabase = createClient(supabaseUrl, supabaseKey);
+  "sb_publishable_2Jwn9mDZ2nTFOG-mXyRGNg__W23jbCW";
+
+  const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
+
+
+// NEXT_PUBLIC_SUPABASE_URL=https://wglexvrxjqbkyhxomsid.supabase.co
+// NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY=sb_publishable_2Jwn9mDZ2nTFOG-mXyRGNg__W23jbCW
